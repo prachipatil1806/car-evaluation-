4a31a0c60de95c57ef33b4eae50cb662f65be209
prachipatil1806 <159413305+prachipatil1806@users.noreply.github.com>
2025-04-08 14:56:32 +0530

    Update deployment_of_car_data.py

--- deployment_of_car_data.py
+++ deployment_of_car_data.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1Rp960QJMQ0uxI75uSUJq3kljPxnlW6lU
 """
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
